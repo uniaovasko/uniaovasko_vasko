63b110e05103dae94b1f62340df679f3a5b32609
Mateus Andrade <m230806@dac.unicamp.br>
2023-11-24 22:32:41 -0300

    ingredientes semi-agrupados

--- project-2-final/src/ligador.py
+++ project-2-final/src/ligador.py
@@ -1,30 +1,55 @@
 import csv
 from textdistance import hamming
 
+def checar_par(ing_cdb, ing_fdb, sem_par: dict[str, str], pares) -> bool:
+    if (s := hamming.normalized_similarity(ing_cdb['Aliased Ingredient Name'].lower(), ing_fdb["name"].lower())) >= 0.85:
+        sem_par.pop(ing_cdb["Entity ID"], None)
+        if ing_cdb['Entity ID'] in pares.keys():
+            print(f"{ing_cdb['Entity ID'], ing_cdb['Aliased Ingredient Name']} de {pares[ing_cdb['Entity ID']]} para {(ing_cdb['Entity ID'], ing_fdb['name'])}")
+        pares[ing_cdb["Entity ID"]] = (ing_cdb['Aliased Ingredient Name'], ing_fdb['id'], ing_fdb["name"], ing_fdb["food_group"], ing_fdb["food_subgroup"])
+        return True
+    return False
 
 def ligar_ingredientes():
     with(open("data/external/02_Ingredients.csv") as ing_cdb_f,
          open("data/external/Food.csv") as ing_fdb_f):
         reader_cdb = list(csv.DictReader(ing_cdb_f))
         reader_fdb = list(csv.DictReader(ing_fdb_f))
-        sem_par = set()
+        sem_par = dict()
         pares = dict()
         for ing_cdb in reader_cdb:
-            sem_par.add(ing_cdb["Aliased Ingredient Name"])
+            sem_par[ing_cdb["Entity ID"]] = ing_cdb["Aliased Ingredient Name"]
             for ing_fdb in reader_fdb:
                 nome_cdb = ing_cdb["Aliased Ingredient Name"].lower()
                 nome_fdb = ing_fdb["name"].lower()
-                if (s := hamming.normalized_similarity(nome_cdb, nome_fdb)) >= 0.85:
-                    sem_par.remove(ing_cdb["Aliased Ingredient Name"])
-                    if ing_cdb["Aliased Ingredient Name"] in pares.keys():
-                        print(f"{ing_cdb['Aliased Ingredient Name']} de {pares[ing_cdb['Aliased Ingredient Name']]} para {ing_fdb['name']}")
-                    pares[ing_cdb["Aliased Ingredient Name"]] = (ing_fdb["id"], ing_fdb["name"])
-                else:
+                if not checar_par(ing_cdb, ing_fdb, sem_par, pares):
                     alt_list = ing_cdb["Ingredient Synonyms"].split("; ")
-        print(len(sem_par) ,sem_par)
+                    novos = []
+                    for x in alt_list:
+                        if x.find("-") > -1:
+                            novos.append(x.replace("-", " "))
+                        if x.find("=") > -1:
+                            novos.append(x.replace("=", " "))
+                            novos.append(x.replace("=", ""))
+                        if x.find("#") > -1:
+                            novos.append(x.removeprefix("#").removesuffix("#"))
+                    alt_list.extend(novos)
+                    for x in alt_list:
+                        if checar_par(ing_cdb, ing_fdb, sem_par, pares):
+                            break
+                            
+        print("Quantidade sem par:", len(sem_par))
+        #print(pares)
         with open("data/interim/ingredientes.csv", "w") as out:
-            pass
-
+            csv_writer = csv.writer(out)
+            csv_writer.writerow(["id_cdb", "nome_cdb", "id_fdb", "nome_cdb", "grupo", "subgrupo"])
+            for (id_cdb, (nome_cdb, id_fdb, nome_fdb, grupo, subgrupo)) in pares.items():
+                csv_writer.writerow([id_cdb, nome_cdb, id_fdb, nome_fdb, grupo, subgrupo])
+        with open("data/interim/ingredientes_sem_nome.csv", "w") as out:
+            csv_writer = csv.writer(out)
+            csv_writer.writerow(["id_cdb", "nome_cdb", "id_fdb", "nome_fdb"])
+            for (id_cdb, nome_cdb) in sem_par.items():
+                csv_writer.writerow([id_cdb, nome_cdb])
 
 
 if __name__ == "__main__":
